9872d3ec85f9536768d29785db2da1cdcf9476a9
Riskbarrabajaer <128870383+Riskbarrabajaer@users.noreply.github.com>
2024-04-09 19:28:23 -0400

    Update Lab5.java

--- src/main/java/com/mycompany/lab5/Lab5.java
+++ src/main/java/com/mycompany/lab5/Lab5.java
@@ -8,13 +8,10 @@ package com.mycompany.lab5;
  * @author HP
  */
 public class Lab5 {
-  
-
-
     public static void main(String[] args) {
         System.out.println("Hello World!");
         Menu menu = new Menu();
         menu.menu_op();
-        
     }
+  
 }
