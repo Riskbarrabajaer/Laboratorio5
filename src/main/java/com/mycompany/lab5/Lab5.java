3a8a09e640684a6aa695cc8bd986294bfa3e48eb
Riskbarrabajaer <128870383+Riskbarrabajaer@users.noreply.github.com>
2024-04-09 21:35:17 -0400

    Update Lab5.java

--- src/main/java/com/mycompany/lab5/Lab5.java
+++ src/main/java/com/mycompany/lab5/Lab5.java
@@ -9,7 +9,6 @@ package com.mycompany.lab5;
  */
 public class Lab5 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         Menu menu = new Menu();
         menu.menu_op();
     }
